a0bba8ecaa27a924b4237982514a3f22deef70e8
Chew Boon Heng <boonheng85@gmail.com>
2018-09-25 23:44:41 +0800

    added initial test

--- microservices.video-search/src/test/kotlin/io/boonlogic/boku/microservices/video/web/handlers/ConfigHandlerTest.kt
+++ microservices.video-search/src/test/kotlin/io/boonlogic/boku/microservices/video/web/handlers/ConfigHandlerTest.kt
@@ -1,52 +1,117 @@
 package io.boonlogic.boku.microservices.video.web.handlers
 
-import io.boonlogic.boku.microservices.video.web.Routes
+import org.assertj.core.api.Assertions
+import org.jooq.DSLContext
+import org.jooq.SQLDialect
+import org.jooq.impl.DSL
+import org.jooq.tools.jdbc.MockConnection
+import org.jooq.tools.jdbc.MockFileDatabase
+import org.junit.Before
 import org.junit.Test
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.extension.ExtendWith
-import org.junit.runner.RunWith
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import org.springframework.test.context.junit4.SpringRunner
-import org.springframework.test.web.reactive.server.WebTestClient
 
-@RunWith(SpringRunner::class)
 @Nested
 @DisplayName("a handler")
-//@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
-@WebFluxTest(controllers = [Routes::class])
+@SpringBootTest
 @ExtendWith(SpringExtension::class)
 class ApplicationTests {
 
-    @Autowired
-    lateinit var client: WebTestClient
+//    private val create by lazy {
+//        val provider = object : MockDataProvider {
+//            override fun execute(ctx: MockExecuteContext?): Array<MockResult> {
+//                val create = DSL.using(SQLDialect.POSTGRES)
+//
+//                println("helloworld")
+//
+//                val mockResults = arrayOf(
+//                    MockResult(
+//                        1,
+//                        create.newResult(DSL.field("name"), DSL.field("description")).apply {
+//                            add(create.newRecord(
+//                                DSL.field("name"), DSL.field("description"))
+//                                .values("Jack Reacher","Tim Cruise movie")
+//                            )
+//                            add(create.newRecord(
+//                                DSL.field("name"), DSL.field("description"))
+//                                .values("Priest","Vampire movie")
+//                            )
+//                            add(create.newRecord(
+//                                DSL.field("name"), DSL.field("description"))
+//                                .values("Police Story","Jackie Chan, who also directs, stars as Kevin, a Hong Kong cop")
+//                            )
+//                        }
+//                    ))
+//
+//                ctx?.let {
+//                    val sql = ctx.sql()
+//                    if(sql.toUpperCase().startsWith("SELECT ")) {
+//                        return mockResults
+//                    } else {
+//                        throw SQLException("Statement not supported: $sql")
+//                    }
+//                }
+//                return emptyArray()
+//            }
+//        }
+//        DSL.using(MockConnection(provider), SQLDialect.POSTGRES)
+//    }
 
-//    @Autowired private lateinit var handler: ConfigHandler
-//    @Autowired private lateinit var restTemplate: TestRestTemplate
+    private val create: DSLContext by lazy {
+        val provider = MockFileDatabase(
+            """
+                select title, desc from video where title = 'hero1';
+                > title            desc
+                > ------------     ----------
+                > Jack Reacher     Tim Cruise movie
+                > Police Story     Jackie Chan, who also directs, stars as Kevin
+                @ rows: 2
 
+                select title, desc from video where title = 'hero2';
+                > title           desc
+                > ------------    ----------
+                > Priest          Vampire movie
+                > Jack Reacher    Tim Cruise movie
+                @ rows: 2
+            """.trimIndent())
+        DSL.using(MockConnection(provider), SQLDialect.POSTGRES)
+    }
+
+    @Before
+    fun setup() {
+
+    }
 
 
     @Test
     fun `run without test`() {
 
-//        val req = ServerResponse.ok().toMono()
-//        handler.foo(req)
+        val result =
+            create.select(DSL.field("title"),DSL.field("desc"))
+                .from(DSL.table("video"))
+                .where(DSL.field("title").eq("hero1"))
+                .fetch {
+                    "${it[0]}|_|${it[1]}"
+                }
+        println(result)
+        Assertions.assertThat("").isEqualTo("")
+    }
 
-        client.get().uri("demo").exchange().expectStatus().isOk
+    @Test
+    fun `run with test`() {
 
-//        client.get()
-//            .uri("/hello")
-//            .exchange()
-//            .expectStatus().isOk
-//            .expectBody()
-//            .consumeWith({ m ->
-//                assertEquals("Hello World!", String(m.responseBodyContent ?: ByteArray(0), StandardCharsets.UTF_8))
-//            })
-
-//        val content = """[{"firstName":"Jack","lastName":"Bauer","id":1},{"firstName":"Chloe","lastName":"O'Brian","id":2},{"firstName":"Kim","lastName":"Bauer","id":3},{"firstName":"David","lastName":"Palmer","id":4},{"firstName":"Michelle","lastName":"Dessler","id":5}]"""
-//        assertEquals(content, restTemplate.getForObject<String>("/customers", String::class.java))
+        val result =
+            create.select(DSL.field("title"),DSL.field("desc"))
+                .from(DSL.table("video"))
+                .where(DSL.field("title").eq("hero2"))
+                .fetch {
+                    "${it[0]}|_|${it[1]}"
+                }
+        println(result)
+        Assertions.assertThat("").isEqualTo("")
     }
 
 }
