a2a54a33c06e48e19e392d601758da9559326ad5
Chew Boon Heng <boonheng85@gmail.com>
2018-09-30 19:51:24 +0800

    added additional test sample

--- microservices.video-search/src/test/kotlin/io/boonlogic/boku/microservices/video/web/handlers/ConfigHandlerTest.kt
+++ microservices.video-search/src/test/kotlin/io/boonlogic/boku/microservices/video/web/handlers/ConfigHandlerTest.kt
@@ -2,10 +2,14 @@ package io.boonlogic.boku.microservices.video.web.handlers
 
 import org.assertj.core.api.Assertions
 import org.jooq.DSLContext
+import org.jooq.Result
 import org.jooq.SQLDialect
 import org.jooq.impl.DSL
 import org.jooq.tools.jdbc.MockConnection
+import org.jooq.tools.jdbc.MockDataProvider
+import org.jooq.tools.jdbc.MockExecuteContext
 import org.jooq.tools.jdbc.MockFileDatabase
+import org.jooq.tools.jdbc.MockResult
 import org.junit.Before
 import org.junit.Test
 import org.junit.jupiter.api.DisplayName
@@ -80,6 +84,41 @@ class ApplicationTests {
         DSL.using(MockConnection(provider), SQLDialect.POSTGRES)
     }
 
+    fun dataMock(
+        expectedSQL: String,
+        validResult: Result<*> = DSL.using(SQLDialect.POSTGRES).newResult(
+            DSL.using(SQLDialect.POSTGRES).newRecord(DSL.field("title"), DSL.field("desc"))
+        ),
+        invaildResult: Result<*> = DSL.using(SQLDialect.POSTGRES).newResult()
+    ): DSLContext {
+        val provider = object : MockDataProvider {
+            override fun execute(ctx: MockExecuteContext?): Array<MockResult> {
+                val sql = ctx?.sql() ?: ""
+                val bindedSQL = DSL.using(SQLDialect.POSTGRES).query(sql, *ctx?.bindings())
+
+
+                println(bindedSQL.toString())
+                println(expectedSQL)
+                println(expectedSQL == bindedSQL.toString())
+
+                return if(expectedSQL == bindedSQL.toString()) {
+                    arrayOf(MockResult(1, validResult))
+                } else {
+                    arrayOf(MockResult(0, validResult))
+                }
+            }
+        }
+        return DSL.using(MockConnection(provider), SQLDialect.POSTGRES)
+    }
+
+    fun testFunction(create: DSLContext) =
+        create.select(DSL.field("title"),DSL.field("desc"))
+            .from(DSL.table("video"))
+            .where(DSL.field("title").eq("hero1"))
+            .fetch {
+                "${it[0]}|_|${it[1]}"
+            }
+
     @Before
     fun setup() {
 
@@ -89,15 +128,9 @@ class ApplicationTests {
     @Test
     fun `run without test`() {
 
-        val result =
-            create.select(DSL.field("title"),DSL.field("desc"))
-                .from(DSL.table("video"))
-                .where(DSL.field("title").eq("hero1"))
-                .fetch {
-                    "${it[0]}|_|${it[1]}"
-                }
-        println(result)
-        Assertions.assertThat("").isEqualTo("")
+        val expectedSQL = "select title, desc from video where title = 'hero1'"
+        val resultSize = testFunction(dataMock(expectedSQL)).size
+        Assertions.assertThat(resultSize).isEqualTo(1)
     }
 
     @Test
